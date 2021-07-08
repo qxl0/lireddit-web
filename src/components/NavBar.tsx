9a96ae8aea7873dea46d5c475bf10d9571abedab
Qiang <qxl0@yahoo.com>
2021-07-08 14:43:19 -0400

    added email to user ui

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import React from 'react'
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
 
 
 interface NavBarProps {
@@ -10,8 +11,12 @@ interface NavBarProps {
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
     const [{fetching: logoutFetching},logout] = useLogoutMutation()
-    const [{data, fetching}] = useMeQuery()
+    const [{data, fetching}] = useMeQuery({
+        pause: isServer(),
+    });
     let body = null
+
+
     if (fetching){
         body = null;
     } else if (!data?.me){
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import {Form, Formik} from 'formik'
-import { Wrapper } from '../components/wrapper';
-import { InputField } from '../components/InputField';
-import { useMutation } from 'urql';
 import { Box, Button } from '@chakra-ui/react';
-import { useRegisterMutation } from '../generated/graphql';
-import { toErrorMap } from '../utils/toErrorMap';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/dist/client/router';
+import React from 'react';
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/wrapper';
+import { useRegisterMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {
 }
@@ -16,9 +17,9 @@ const Register: React.FC<registerProps> = ({}) => {
     const [,register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
-        <Formik initialValues={{username: "", password: ""}} 
+        <Formik initialValues={{email: "", username: "", password: ""}} 
             onSubmit={async (values, { setErrors }) => {
-                const response = await register(values)
+                const response = await register({ options: values })
                 console.log(response);
                 if (response.data?.register.errors){
                   setErrors(toErrorMap(response.data.register.errors));
@@ -32,6 +33,9 @@ const Register: React.FC<registerProps> = ({}) => {
                 <Form>
                     <InputField name="username" placeholder="username" label="Username"/>
                     <Box mt={4}>
+                        <InputField name="email" placeholder="email" label="Email"/>
+                    </Box>
+                    <Box mt={4}>
                         <InputField name="password" placeholder="password" label="Password" type="password"/>
                     </Box>
                     <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme="teal">register</Button>
@@ -42,4 +46,4 @@ const Register: React.FC<registerProps> = ({}) => {
     );
 }
 
-export default  Register
+export default  withUrqlClient(createUrqlClient)(Register);
