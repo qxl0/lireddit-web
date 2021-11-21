d2f1e0b786749d352a526b510398d8b111dcd709
Qiang <qxl0@yahoo.com>
2021-11-21 14:36:48 -0500

    readfragments/writefragments => vote change

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,11 +1,12 @@
 import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
-import { dedupExchange, fetchExchange, stringifyVariables } from "urql";
+import { dedupExchange, fetchExchange, gql, stringifyVariables } from "urql";
 import {
   LogoutMutation,
   MeQuery,
   MeDocument,
   LoginMutation,
   RegisterMutation,
+  VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 
@@ -60,6 +61,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+            fragment __ on Post {
+              id
+              points
+            }
+            `,
+              { id: postId }
+            );
+            if (data) {
+              const newPoints = data.points + value;
+              cache.writeFragment(
+                gql`
+            fragment __ on Post {
+              points
+            }
+            `,
+                { id: postId, points: newPoints }
+              );
+            }
+          },
           createPost:(_rsult, args, cache, info) => {
             const allFields = cache.inspectFields("Query");
             const fieldInfos = allFields.filter((info) => info.fieldName === "posts");
